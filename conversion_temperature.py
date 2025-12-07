73d10db13c43fba43578ddf95a8a3f8fe597dfb4
vboxuser <vboxuser@Ubuntu2.0.myguest.virtualbox.org>
2025-12-07 14:03:52 +0100

    ajout exercice 1_2_conversion_temeperature.py

---
+++ conversion_temperature.py
@@ -0,0 +1,4 @@
+#convertir la temperature
+celsius = float(input("Entrez la température en Celsius : "))
+fahrenheit = (celsius * 9/5) + 32
+print (f"la temperature en fahrenheit est: {fahrenheit}")
