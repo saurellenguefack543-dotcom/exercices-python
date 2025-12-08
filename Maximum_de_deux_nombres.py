2c19c8d4065a722600c64b53cf318a861003dd74
vboxuser <vboxuser@Ubuntu2.0.myguest.virtualbox.org>
2025-12-08 18:43:53 +0100

    Maximum_de_deux_nombres.py

---
+++ Maximum_de_deux_nombres.py
@@ -0,0 +1,10 @@
+
+a = int(input("Entrez le premier nombre : "))
+b = int(input("Entrez le second nombre : "))
+if a > b:
+    print("Le maximum est :", a)
+elif b > a:
+    print("Le maximum est :", b)
+else:
+    print("Les deux nombres sont égaux :", a)
+
