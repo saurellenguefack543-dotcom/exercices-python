cd69530aeb04c196577cb5c4638b5c98da7e6136
vboxuser <vboxuser@Ubuntu2.0.myguest.virtualbox.org>
2025-12-09 00:06:21 +0100

    convertisseur_t.py

---
+++ convertisseur_t.py
@@ -0,0 +1,9 @@
+def c_to_f(c):
+    return (c * 9/5) + 32
+
+def f_to_c(f):
+    return (f - 32) * 5/9
+
+# Exemples d'utilisation
+print(c_to_f(25))   # Celsius → Fahrenheit
+print(f_to_c(25))   # Fahrenheit → Celsius
