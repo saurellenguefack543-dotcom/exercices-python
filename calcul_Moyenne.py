911d06a9645146248372da5a5afab7b06e585112
vboxuser <vboxuser@Ubuntu2.0.myguest.virtualbox.org>
2025-12-08 17:58:08 +0100

    calcul_Moyenne.py

---
+++ calcul_Moyenne.py
@@ -0,0 +1,6 @@
+note1 = float(input("Entrez une premiere note: "))
+note2 = float(input("Entrz une seconde note: "))
+note3 = float(input("Entrez une troisieme note: "))
+
+moyenne = (note1 + note2 + note3) / 3 
+print("la moyenne est :", moyenne)
